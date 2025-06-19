4ada23a8135bc93e7b07f7cb04fb0b3a8a34c556
rivethead13 <77887958+rivethead13@users.noreply.github.com>
2025-06-19 16:21:00 -0400

    Update streamlit_app.py
    corrected to read json

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ if time_to_insert:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
