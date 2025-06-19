4fef9d3387e4591ca157030f343f3ca57c949ba5
rivethead13 <77887958+rivethead13@users.noreply.github.com>
2025-06-19 18:14:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,10 +41,10 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-  #  st.write(ingredients_string)
-        time_to_insert = st.button('Submit Order')
+         #  st.write(ingredients_string)
+time_to_insert = st.button('Submit Order')
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 #st.write(my_insert_stmt)
 
 
