a90f01c0e061037353a6f20eaa6fbd6a272501e5
rivethead13 <77887958+rivethead13@users.noreply.github.com>
2025-06-19 19:03:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-ingredients_string = ''.join(ingredients_list)
+ingredients_string = ' '.join(ingredients_list)
 
 if ingredients_list:
     for fruit_chosen in ingredients_list:
