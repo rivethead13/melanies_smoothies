ff6e30e2ed965e73abc820bba7de2205fe8aedd7
rivethead13 <77887958+rivethead13@users.noreply.github.com>
2025-06-19 16:16:50 -0400

    Update streamlit_app.py
    adding in requests lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,8 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
