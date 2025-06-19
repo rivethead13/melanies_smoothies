5c0268e3d05a6167f09e060be4bdabc32f3012e2
rivethead13 <77887958+rivethead13@users.noreply.github.com>
2025-06-19 16:25:21 -0400

    Update streamlit_app.py
    fixing damn initalization bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,10 @@ ingredients_list = st.multiselect(
     my_dataframe
     ,max_selections=5
 )
+ingredients_string = ''
 if ingredients_list:
 
-    ingredients_string = ''
+   
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
